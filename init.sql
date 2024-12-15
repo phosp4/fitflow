3e4c1ed8ac479f9ccb3592cb0ee20c17b346148b
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-12-15 18:09:47 +0100

    Add salt to the user

--- init.sql
+++ init.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS users
     id             INTEGER PRIMARY KEY AUTOINCREMENT,
     role           INTEGER   NOT NULL,
     email          TEXT      NOT NULL,
+    salt           TEXT      NOT NULL,
     password_hash  TEXT      NOT NULL,
     first_name     TEXT      NOT NULL,
     last_name      TEXT      NOT NULL,
