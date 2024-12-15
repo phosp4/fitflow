c49fedda6ebb8427964633ac44049a4a69b02bef
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-12-15 18:40:12 +0100

    Fix a typo in init.sql

--- init.sql
+++ init.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS trainers_intervals;
 DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS reservation_statuses;
 DROP TABLE IF EXISTS visits;
-DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS transaction_types;
+DROP TABLE IF EXISTS credit_transactions;
+DROP TABLE IF EXISTS credit_transaction_types;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS users
     FOREIGN KEY (role) REFERENCES roles (id)
 );
 
-CREATE TABLE IF NOT EXISTS credit_ttransaction_types
+CREATE TABLE IF NOT EXISTS credit_transaction_types
 (
     id   INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
@@ -41,14 +41,14 @@ CREATE TABLE IF NOT EXISTS credit_ttransaction_types
 
 CREATE TABLE IF NOT EXISTS credit_transactions
 (
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id             INT       NOT NULL,
-    amount              REAL,
-    transaction_type_id INTEGER   NOT NULL,
-    created_at          TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    updated_at          TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    id                          INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id                     INT       NOT NULL,
+    amount                      REAL,
+    credit_transaction_type_id  INTEGER   NOT NULL,
+    created_at                  TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    updated_at                  TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (transaction_type_id) REFERENCES transaction_types (id)
+    FOREIGN KEY (credit_transaction_type_id) REFERENCES credit_transaction_types (id)
 );
 
 CREATE TABLE IF NOT EXISTS visits
@@ -58,11 +58,11 @@ CREATE TABLE IF NOT EXISTS visits
     check_in_time  DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     check_out_time DATETIME,
     visit_secret   TEXT      NOT NULL,
-    transaction_id INTEGER   NOT NULL,
+    credit_transaction_id INTEGER   NOT NULL,
     created_at     TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     updated_at     TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (transaction_id) REFERENCES transactions (id)
+    FOREIGN KEY (credit_transaction_id) REFERENCES credit_transactions (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation_statuses
@@ -79,10 +79,10 @@ CREATE TABLE IF NOT EXISTS reservations
     note           TEXT,
     created_at     TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     updated_at     TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    transaction_id INTEGER   NOT NULL,
+    credit_transaction_id INTEGER   NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES users (id),
     FOREIGN KEY (status) REFERENCES reservation_statuses (id),
-    FOREIGN KEY (transaction_id) REFERENCES transactions (id)
+    FOREIGN KEY (credit_transaction_id) REFERENCES credit_transactions (id)
 );
 
 CREATE TABLE IF NOT EXISTS trainers_intervals
@@ -117,7 +117,7 @@ VALUES ('admin'),
        ('user'),
        ('trainer');
 
-INSERT INTO transaction_types (name)
+INSERT INTO credit_transaction_types (name)
 VALUES ('purchase'),
        ('refund');
 
@@ -131,12 +131,12 @@ VALUES ('Yoga'),
        ('Pilates'),
        ('CrossFit');
 
-INSERT INTO users (id, role, email, password_hash, first_name, last_name, credit_balance, phone, birth_date, active, created_at, updated_at) VALUES
-(1, 1, 'user1@example.com', '$2b$10$xY/j5.25h8xL0t5c.jR7Bu.5x74j9.2gV9/0/j109.4e1Z9.3160', 'John', 'Doe', 100.00, '123-456-7890', '1990-01-01', 1, '2023-11-23 12:34:56', '2023-11-23 12:34:56'),
-(2, 2, 'user2@example.com', '$2b$10$xY/j5.25h8xL0t5c.jR7Bu.5x74j9.2gV9/0/j109.4e1Z9.3160', 'Jane', 'Smith', 50.50, '987-654-3210', '1995-02-15', 1, '2023-11-23 12:34:56', '2023-11-23 12:34:56'),
-(3, 3, 'admin@example.com', '$2b$10$xY/j5.25h8xL0t5c.jR7Bu.5x74j9.2gV9/0/j109.4e1Z9.3160', 'Alice', 'Johnson', 0.00, '555-555-5555', '2000-03-30', 1, '2023-11-23 12:34:56', '2023-11-23 12:34:56');
+INSERT INTO users (id, role, email, salt, password_hash, first_name, last_name, credit_balance, phone, birth_date, active, created_at, updated_at) VALUES
+(1, 1, 'user1@example.com', '123456', '$2b$10$xY/j5.25h8xL0t5c.jR7Bu.5x74j9.2gV9/0/j109.4e1Z9.3160', 'John', 'Doe', 100.00, '123-456-7890', '1990-01-01', 1, '2023-11-23 12:34:56', '2023-11-23 12:34:56'),
+(2, 2, 'user2@example.com', '123456', '$2b$10$xY/j5.25h8xL0t5c.jR7Bu.5x74j9.2gV9/0/j109.4e1Z9.3160', 'Jane', 'Smith', 50.50, '987-654-3210', '1995-02-15', 1, '2023-11-23 12:34:56', '2023-11-23 12:34:56'),
+(3, 3, 'admin@example.com', '123456', '$2b$10$xY/j5.25h8xL0t5c.jR7Bu.5x74j9.2gV9/0/j109.4e1Z9.3160', 'Alice', 'Johnson', 0.00, '555-555-5555', '2000-03-30', 1, '2023-11-23 12:34:56', '2023-11-23 12:34:56');
 
-INSERT INTO reservations (id, customer_id, status, note, created_at, updated_at, transaction_id) VALUES
+INSERT INTO reservations (id, customer_id, status, note, created_at, updated_at, credit_transaction_id) VALUES
 (1, 10, 'pending', 'Please focus on core strength exercises.', '2023-11-23 12:34:56', '2023-11-23 12:34:56', 100),
 (2, 20, 'confirmed', 'No special requests.', '2023-11-24 10:00:00', '2023-11-24 10:00:00', 200),
 (3, 10, 'canceled', 'Canceled due to illness.', '2023-11-25 14:00:00', '2023-11-25 14:00:00', 300);
@@ -146,12 +146,12 @@ INSERT INTO trainers_intervals (id, trainer_id, day, start_time, end_time, reser
 (200, 2, '2023-11-24', '10:00:00', '11:30:00', 2),
 (300, 1, '2023-11-25', '14:00:00', '15:15:00', 3);
 
-INSERT INTO transactions (id, user_id, amount, transaction_type_id, created_at) VALUES
+INSERT INTO credit_transactions (id, user_id, amount, credit_transaction_type_id, created_at) VALUES
 (100, 10, 10.00, 1, '2023-11-23 12:34:56'),
 (200, 20, 20.00, 2, '2023-11-24 10:00:00'),
 (300, 10, 30.00, 1, '2023-11-25 14:00:00');
 
-INSERT INTO visits (id, user_id, check_in_time, check_out_time, visit_secret, transaction_id) VALUES
+INSERT INTO visits (id, user_id, check_in_time, check_out_time, visit_secret, credit_transaction_id) VALUES
 (1, 10, '2023-11-23 12:34:56', '2023-11-23 13:45:00', 'secret123', 100),
 (2, 20, '2023-11-24 10:00:00', '2023-11-24 11:30:00', 'secret456', 200),
 (3, 10, '2023-11-25 14:00:00', '2023-11-25 15:15:00', 'secret789', 300);
