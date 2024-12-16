3c5fc7ca2b8adc47dcbb294806ba417b9461c7cc
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-12-16 15:30:50 +0100

    Start implementing SQLReservationStatusDao

--- src/main/java/sk/upjs/ics/daos/interfaces/ReservationStatusDao.java
+++ src/main/java/sk/upjs/ics/daos/interfaces/ReservationStatusDao.java
@@ -2,9 +2,12 @@ package sk.upjs.ics.daos.interfaces;
 
 import sk.upjs.ics.entities.ReservationStatus;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public interface ReservationStatusDao {
+
+    void loadFromCsv(File file);
 
     void create(String status);
 
