39b7b080b6cb9d7701dea19414f400e68d1992ad
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-12-16 13:35:15 +0100

    Finish SQLCreditTransactionDao

--- src/main/java/sk/upjs/ics/daos/interfaces/CreditTransactionDao.java
+++ src/main/java/sk/upjs/ics/daos/interfaces/CreditTransactionDao.java
@@ -2,7 +2,11 @@ package sk.upjs.ics.daos.interfaces;
 
 import sk.upjs.ics.entities.CreditTransaction;
 
+import java.io.File;
+
 public interface CreditTransactionDao {
+
+    void loadFromCsv(File file);
 
     void create(CreditTransaction creditTransaction);
 
