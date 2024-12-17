fa2e393f3a924ecd0e4e4c2d5f3c2b5e51f8dd91
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-12-17 05:51:12 +0100

    Add scaffold

--- src/main/java/sk/upjs/ics/controllers/ReservationViewController.java
+++ src/main/java/sk/upjs/ics/controllers/ReservationViewController.java
@@ -9,6 +9,11 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import sk.upjs.ics.Factory;
+import sk.upjs.ics.daos.interfaces.ReservationDao;
+import sk.upjs.ics.daos.interfaces.TrainerIntervalDao;
+import sk.upjs.ics.daos.interfaces.UserDao;
+import sk.upjs.ics.entities.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +22,14 @@ public class ReservationViewController implements Initializable {
 
 //    @FXML
 //    private MFXToggleButton TestToggle;
+    private final UserDao userDao = Factory.INSTANCE.getUserDao();
+    private final TrainerIntervalDao trainerIntervalDao = Factory.INSTANCE.getTrainerIntervalDao();
+    private final ReservationDao reservationDao = Factory.INSTANCE.getReservationDao();
+
+    private User selectedTrainer;
+
+    @FXML
+    private MFXToggleButton TestToggle;
 
     @FXML
     private MFXButton createReservationButton;
@@ -35,12 +48,23 @@ public class ReservationViewController implements Initializable {
 
     @FXML
     void createReservation(ActionEvent event) {
-        // vytvorit nejakych trenerov a dat im intervaly
-        // nacitat ich do comboboxov
+        String selectedTrainer = reserveTrainerComboBox.getValue();
+        String selectedTime = reserveTimeComboBox.getValue();
+        String selectedDate = reserveDatePicker.getValue().toString();
+        String note = noteToTrainer.getText();
+
+        if (selectedTrainer == null) {
+            System.out.println("No trainer selected");
+            return;
+        }
+
+        System.out.println("The user has selected " + selectedTrainer + " on " + selectedDate + " at " + selectedTime);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        reserveTrainerComboBox.getItems().addAll("Trainer 1", "Trainer 2", "Trainer 3", "Trainer 4", "Trainer 5");
+
         // demo
         reserveTimeComboBox.getItems().addAll("10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00");
         Platform.runLater(() -> reserveTimeComboBox.setValue("10:00"));
